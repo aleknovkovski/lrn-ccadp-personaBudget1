bd8d2768f6b4ae8796087c0bee631b4dc5bd343d
Alek Novkovski <alek@greatcode2.com>
2023-05-15 20:10:09 +0200

    Setup envelopes router

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
-app.get('/', (req, res) => {
-   res.send("Hello World")
-})
+const envelopesRouter = require('./routes/envelopes')
+
+app.use("/api/v1/envelopes", envelopesRouter);
 app.listen(PORT,()=>{
    console.log("Server is listening on port 3000")
 })
