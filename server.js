8f4403f3fa98e26a64bf82924a4ee74fa0fbd5a1
Alek Novkovski <alek@greatcode2.com>
2023-05-15 19:42:37 +0200

    Change port to 3000, send hello world on root

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
+const PORT = 3000
 
-app.listen(5000,()=>{
-   console.log("Server is listening on port 5000")
+app.get('/', (req, res) => {
+   res.send("Hello World")
+})
+app.listen(PORT,()=>{
+   console.log("Server is listening on port 3000")
 })
