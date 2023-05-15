b1403a1b1068c6025961f141089ae244943ca349
Alek Novkovski <alek@greatcode2.com>
2023-05-15 20:05:34 +0200

    Add a fake 'database'

---
+++ db.js
@@ -0,0 +1,21 @@
+// This file approximates, or acts as a fake database
+
+const envelopes = [
+  {
+    id: 1,
+    title: "Dance Parties",
+    budget: 150,
+  },
+  {
+    id: 2,
+    title: "Fun Food",
+    budget: 200,
+  },
+  {
+    id: 3,
+    title: "Dating Related",
+    budget: 400,
+  },
+];
+
+module.exports = envelopes;
